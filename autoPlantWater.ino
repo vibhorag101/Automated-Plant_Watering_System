947162d345f9baadbc65721851b4e7f4c43b6343
VIBHOR AGARWAL <76804249+vibhorag101@users.noreply.github.com>
2021-05-03 08:51:57 +0530

    implemented LCD Welcome Screen

--- autoPlantWater.ino
+++ autoPlantWater.ino
@@ -1,28 +1,47 @@
 // we have calibrated the sensor taking analogRead 300 as wettest soil and 1000 as dryest soil
-# define sensorPin A0 // analog pin of the soil sensor is initialised to A0
+#include <LiquidCrystal_I2C.h>
+#define sensorPin A0 // analog pin of the soil sensor is initialised to A0
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 int moistureLevel; // the analog resistor output representing the moisture level
 int moisturePercent;
-void setup(){
-pinMode(sensorPin,INPUT); // A0 of arduino is initialised as output
-Serial.begin(9600);
+void setup()
+{
+    pinMode(sensorPin, INPUT); // A0 of arduino is initialised as output
+    Serial.begin(9600);
+    welcomeLCD();
+    
 }
-void loop(){
-    moistureLevel= moistureLevelFinder(sensorPin);
-    moisturePercent=moisturePercentFinder(moistureLevel);
+void loop()
+{
+    moistureLevel = moistureLevelFinder(sensorPin);
+    moisturePercent = moisturePercentFinder(moistureLevel);
     Serial.println(moistureLevel);
     Serial.println(moisturePercent);
-    
-    delay(1000); 
+
+    delay(1000);
 }
 //function for calculating the humidity level of the soil
-int moistureLevelFinder(int analogPin){ // input pin having analog input
-    moistureLevel=analogRead(analogPin); 
+int moistureLevelFinder(int analogPin)
+{ // input pin having analog input
+    moistureLevel = analogRead(analogPin);
     return moistureLevel;
 }
 /* function for calculating the humidity percentage by taking 300 as the lower mark for wetness and
  taking 1000 as the upper mark for the dryness.
  */
-int moisturePercentFinder(int moistureLevel){
-    int moisturePercent= (1000-moistureLevel)*(0.14); //  0.14 come by dividing 100 by (1000-300)
+int moisturePercentFinder(int moistureLevel)
+{
+    int moisturePercent = (1000 - moistureLevel) * (0.14); //  0.14 come by dividing 100 by (1000-300)
     return moisturePercent;
+}
+
+void welcomeLCD(){
+    lcd.init();
+    lcd.clear();
+    lcd.backlight();
+    lcd.setCursor(0,0);
+    lcd.print("WELCOME TO AUTO ");
+    lcd.setCursor(0,1);
+    lcd.print("PLANT WATERING");
+
 }
